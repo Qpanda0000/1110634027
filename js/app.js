ba85ab102abee9e524de58e550f976c95f9fa228
Qpanda0000 <80401871+Qpanda0000@users.noreply.github.com>
2022-05-25 16:19:22 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/0525.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/1111.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
